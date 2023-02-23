cf9eef7be8f1506496258099638ef5ad9945232a
XHeapStack <jackey.dou@gmail.com>
2023-02-23 22:50:43 +0800

    feat: use oxford online api & optional chain

--- src/components/word_detail/index.tsx
+++ src/components/word_detail/index.tsx
@@ -12,24 +12,24 @@ export default function WordDetail(props: IProps) {
     <div className="w-full py-2">
       {lexicalEntries.map((entry, idx) => {
         return (
-          <div className="py-2 px-2 cursor-pointer transition-colors hover:bg-gray-50 rounded-md">
-            <div className="flex items-center" key={entry.lexicalCategory.id + idx}>
-              <div className="px-2 py-1 rounded-full bg-teal-100 mr-2">{entry.lexicalCategory.id}</div>
+          <div key={entry?.lexicalCategory?.id + idx} className="py-2 px-2 cursor-pointer transition-colors hover:bg-gray-50 rounded-md">
+            <div className="flex items-center">
+              <div className="px-2 py-1 rounded-full bg-teal-100 mr-2">{entry.lexicalCategory?.id}</div>
               <div>
-                {entry.entries.map((it, idx) => {
+                {entry.entries?.map((it, idx) => {
                   return (
                     <div key={idx}>
-                      {it.pronunciations.map(pronunciation => (<div className="flex items-center" key={pronunciation.phoneticNotation}><SpeakerWaveIcon className="h-4 w-4 text-gray-500 mr-1"/>/ {pronunciation.phoneticSpelling} /</div>))}
+                      {it.pronunciations?.map((pronunciation, index) => (<div className="flex items-center" key={pronunciation?.phoneticNotation + index}><SpeakerWaveIcon className="h-4 w-4 text-gray-500 mr-1"/>/ {pronunciation.phoneticSpelling} /</div>))}
                     </div>
                   )
                 })}
               </div>
             </div>
             <div className="py-2">
-              {entry.entries.map((it, idx) => {
+              {entry.entries?.map((it, idx) => {
                 return (
                   <ul key={idx} className="list-disc pl-6">
-                    {it.senses.map(sense => (<li>{sense.definitions[0]}</li>))}
+                    {it.senses?.map(sense => (<li>{sense?.definitions?.[0]}</li>))}
                   </ul>
                 )
               })}
